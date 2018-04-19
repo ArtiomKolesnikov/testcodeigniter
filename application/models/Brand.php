c180707c893c55c46333d2c663217616b4dfecfc
Artiom <77777green77777@gmail.com>
2018-04-19 23:46:49 +0300

    1

---
+++ application/models/Brand.php
@@ -0,0 +1,22 @@
+<?php
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class Brand extends Eloquent
+
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+
+    protected $guarded = [];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+
+}
